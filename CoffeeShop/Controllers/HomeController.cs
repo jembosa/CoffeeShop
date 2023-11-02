0fd3053940197c3cd81db21df7d6b4744f387d65
Alex Young <alexanderjyoung15@gmail.com>
2023-11-02 17:18:23 -0400

    Add project files.

---
+++ CoffeeShop/Controllers/HomeController.cs
@@ -0,0 +1,88 @@
+﻿using CoffeeShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoffeeShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public CoffeeShopContext db = new CoffeeShopContext();
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //Technically the db.table syntax return an ienumerable which can just call toList on
+            List<Product> products = db.Products.ToList();
+            return View(products);
+        }
+
+        public IActionResult Product(int id)
+        {
+            Product p = db.Products.Find(id);
+            return View(p);
+        }
+
+        //This action is a get that exists to display our view 
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+
+        //This second actions exists to add the movie to our table and redirect us to the main listing
+        [HttpPost]
+        public IActionResult CreateProduct(Product product)
+        {
+            db.Products.Add(product);
+            //This needs to be called after any create, delete or edit actions 
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        //We will use the id to find the movie we want to update
+        public IActionResult UpdateProduct(int id)
+        {
+            Product update = db.Products.Find(id);
+            return View(update);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateProduct(Product p)
+        {
+            db.Products.Update(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //This display a view asking if you are sure you want to delete this movie
+        public IActionResult DeleteProduct(int id)
+        {
+            Product p = db.Products.Find(id);
+            return View(p);
+        }
+
+        public IActionResult IAmSureDeleteProduct(int id)
+        {
+            Product p = db.Products.Find(id);
+            db.Products.Remove(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CoffeeShop/Models/CoffeeShopContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeShop.Models;
+
+public partial class CoffeeShopContext : DbContext
+{
+    public CoffeeShopContext()
+    {
+    }
+
+    public CoffeeShopContext(DbContextOptions<CoffeeShopContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=ALEX-2020BUILD;Database=CoffeeShop;Trusted_Connection=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Products__3213E83F4AE42798");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Category).HasMaxLength(30);
+            entity.Property(e => e.Description).HasMaxLength(20);
+            entity.Property(e => e.Name).HasMaxLength(20);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
